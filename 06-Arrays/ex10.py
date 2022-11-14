8df1f13c20596ef848aa0662618fdbb408a562ef
s-121-26 <s-121-26@gumiforest.ae.krakow.pl>
2022-11-14 12:57:02 +0100

    exercises lesson 6

---
+++ 06-Arrays/ex10.py
@@ -0,0 +1,8 @@
+import random
+
+arr * 10 = []
+
+for a in range(10):
+    arr[a] = random.randint(0,100)
+
+print(arr)
---
+++ 06-Arrays/ex12.py
@@ -0,0 +1,18 @@
+
+arr = [[2,5,4],[9,0,3]]
+
+print(arr)
+
+print(f'rows {len(arr)} columns {len(arr[0])}')
+
+print(arr[0][1])
+
+print(arr[1][2])
+
+print(arr[1])
+
+for table in arr:
+    print(table)
+
+for table in arr:
+    print(table[len(table)-1])
